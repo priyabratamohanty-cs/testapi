35177871fccbe3260af88decb6ef060c4702c484
Priyabrata Mohanty <priyabratamohanty.cs@gmail.com>
2020-02-12 17:47:40 +0530

    2nd commit

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const Router = express.Router();
 const dummy = require('../model/data');
 
 Router.get('/dummyapi', (req, res) => {
- 
+    console.log("true");
     res.send('request reached to server');
 })
 
